6ce1eee2e679fcde45632e5458c78feedd52558a
Marcio Cruz de Almeida <marciocadev@gmail.com>
2024-05-14 18:19:27 -0300

    Initial commit

---
+++ test/oauth2-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Oauth2Aws from '../lib/oauth2-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/oauth2-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Oauth2Aws.Oauth2AwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
